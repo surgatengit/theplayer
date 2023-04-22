fa29aba0dc3f68c6e73feb7a8e88699c53e4cbf7
Surgat <53380435+surgatengit@users.noreply.github.com>
2023-04-22 07:06:35 +0200

    Update theplayer.sh

--- theplayer.sh
+++ theplayer.sh
@@ -12,18 +12,6 @@ fi
 ipvictima=$1
 nombre=$2
 
-# Mas abajo queda solucionado agregar a host urls, si no da problemas borrar
-#echo "[+] Revisando si exite $nombre.htb en el archivo Host"
-#if [ $(cat /etc/hosts | grep -c "$ipvictima $nombre.htb") = 1 ]
-#    then
-#        echo
-#        echo "Existe $ipvictima $nombre.htb en /etc/hosts con lo que no se agregara."
-#    else
-#        echo "Agregando $ipvictima $nombre.htb a /etc/hosts"
-#        echo "$ipvictima $nombre.htb" | sudo tee -a /etc/hosts
-#    fi
-#echo
-
 echo "[+] Escaneo rapido"
 sudo nmap -T4 -F $ipvictima
 
@@ -44,9 +32,6 @@ for url in $urls; do
         echo "Skipping $url because it already exists in /etc/hosts"
     fi
 done
-echo "[+] Buscando URLs en el resultado de nmap..."
-urls=$(grep -oP '(http|https)://[\w\-\.]+\.[a-zA-Z]+(:\d+)?(/[\w/_\.]*)?' ResultNmap$nombre.xml)
-
 if [ -z "$urls" ]
 then
     echo "No se encontraron URLs en el resultado de nmap."
@@ -61,4 +46,4 @@ else
 fi
 
 echo "[+] Searching eXploiTs..."
-gnome-terminal --working-directory="$PWD" --title="searchsploit Results for $nombre" -- "searchsploit --nmap ResultNmap$nombre.xml"
+gnome-terminal -- "searchsploit --nmap ResultNmap$nombre.xml"
