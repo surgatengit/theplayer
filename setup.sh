6a47321596de54c800fb8e9a3b333d5c24b5b0b1
Surgat <53380435+surgatengit@users.noreply.github.com>
2024-10-20 02:13:22 +0200

    Update tools with apt in kali, and limit the use of pip

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 sudo apt update
-sudo apt install python3 python3-dev python3-pip python3-venv nmap smbmap john libsasl2-dev libldap2-dev libkrb5-dev ntpdate wget zip unzip systemd-timesyncd pipx swig curl jq openssl enum4linux-ng -y && sudo apt autoremove -y
+sudo apt install python3 python3-dev python3-pip python3-venv nmap smbmap john coercer netexec libsasl2-dev libldap2-dev libkrb5-dev ntpdate wget zip unzip systemd-timesyncd pipx swig curl jq openssl enum4linux-ng -y && sudo apt autoremove -y
 
-pip3 install --user pipx PyYAML alive-progress xlsxwriter sectools typer --upgrade
+pip install --user pipx --upgrade # Only in virtual enviroment PyYAML alive-progress xlsxwriter sectools typer 
 pipx ensurepath
 pipx install git+https://github.com/dirkjanm/ldapdomaindump.git --force
-pipx install git+https://github.com/Pennyw0rth/NetExec.git --force
 pipx install git+https://github.com/franc-pentest/ldeep.git --force
 pipx install git+https://github.com/garrettfoster13/pre2k.git --force
-pipx install git+https://github.com/p0dalirius/Coercer --force
 pipx install git+https://github.com/p0dalirius/RDWAtool --force
